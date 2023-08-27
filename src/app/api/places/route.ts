988a59a67b612ea3cb56f0b7a752870506a57791
Hugo Freire <ahugo.freire@gmail.com>
2023-08-27 01:39:12 +0000

    refactor code

--- src/app/api/places/route.ts
+++ src/app/api/places/route.ts
@@ -4,7 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const url = new URL(request.url)
   const text = url.searchParams.get("text")
-  const response = await fetch(`${process.env.NEXT_PUBLIC_NEST_API_URL}/places?text${text}`, {
+  console.log(text)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_NEST_API_URL}/places?text=${text}`, {
     next: {
       revalidate: 1, //cache bem grande, valor bem grande
     }
